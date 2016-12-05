7824783db9288250bb30f615b7a1aacd79516ac5
redneb07 <meiershane07@gmail.com>
2016-12-05 11:29:47 -0600

    Add files via upload

--- sample.php
+++ sample.php
@@ -12,7 +12,12 @@
         <h3 style="text-align: center;">Population of School by Students</h3>
         <canvas id="myPieChart" width="200" height="200"></canvas>
        </div>
+	   <div id="chart2">
+        <h3 style="text-align: center;">Spending of School per Student</h3>
+        <canvas id="myPieChart2" width="200" height="200"></canvas>
+       </div>
 		</article>
+	</div>
 		<?php include 'includes\footer.php' ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.min.js"></script>
@@ -48,6 +53,66 @@ var myPieChart = new Chart(ctz, {
 					   totals['Como Park Senior High School'] += element.STUDENT_POPULATION
 				   }else if(element.SCHOOL == "North Senior High School"){
 					   totals['North Senior High School'] += element.STUDENT_POPULATION
+				   }
+			   })
+               return [totals['Woodbury Senior High School'],totals['East Ridge Senior High School'],totals['Tartan Senior High School']
+			   ,totals['Como Park Senior High School'],totals['North Senior High School']];
+           })(),
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(0, 255, 0, 0.8)',
+                'rgba(255, 100, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(255, 255, 64, 0.8)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235,1)',
+                'rgba(0, 255, 0, 1)',
+                'rgba(255, 100, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255, 255, 64, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+<script>
+var ctz = document.getElementById("myPieChart2");
+var myPieChart2 = new Chart(ctz, {
+    type: 'bar',
+    data: {
+        labels: ["Woodbury (200,000,000)", "East Ridge (200,000,000)","Tartan (148,000,000)","Como Park (300,000,000)","North (147,000,000)"],
+        datasets: [{
+            label: 'Spending Per Student',
+            data: (function(){
+			   var totals = {
+				   'Woodbury Senior High School': 0,
+				   'East Ridge Senior High School': 0,
+				   'Tartan Senior High School': 0,
+				   'Como Park Senior High School': 0,
+				   'North Senior High School': 0,
+			   }
+               data.forEach(function(element){
+				   if(element.SCHOOL == "Woodbury Senior High School"){
+					   totals['Woodbury Senior High School'] += element.SPENDING_STUDENT
+				   }else if(element.SCHOOL == "East Ridge Senior High School"){
+					   totals['East Ridge Senior High School'] += element.SPENDING_STUDENT
+				   }else if(element.SCHOOL == "Tartan Senior High School"){
+					   totals['Tartan Senior High School'] += element.SPENDING_STUDENT
+				   }else if(element.SCHOOL == "Como Park Senior High School"){
+					   totals['Como Park Senior High School'] += element.SPENDING_STUDENT
+				   }else if(element.SCHOOL == "North Senior High School"){
+					   totals['North Senior High School'] += element.SPENDING_STUDENT
 				   }
 			   })
                return [totals['Woodbury Senior High School'],totals['East Ridge Senior High School'],totals['Tartan Senior High School']
