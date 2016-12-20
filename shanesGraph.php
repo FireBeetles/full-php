6f99344eb2efb87b59dd8a6dcc9af44b7f65a078
redneb07 <meiershane07@gmail.com>
2016-12-20 11:49:16 -0600

    Add files via upload

--- shanesGraph.php
+++ shanesGraph.php
@@ -23,8 +23,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-	<script src="schoolData.json"></script>
+	<script src="include/schoolData.json"></script>
  <script>
 var ctz = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctz, {
