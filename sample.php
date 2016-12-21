9e21ad9e0b8ebc9b61dd72f39a6cbc038f45a44b
mcnapatm <mcnpatm@dunwoody.edu>
2016-12-21 10:38:46 -0600

    Add files via upload

--- sample.php
+++ sample.php
@@ -16,6 +16,17 @@
 				<script src="js/CPmin.js"></script>
 				<script src="js/NSGrad.js"></script>
 				<script src="js/NSmin.js"></script>
+				<script>
+					function myFunction() {
+						var x = "container"+document.getElementById("SchoolDropDown").value;
+						var y = document.getElementsByClassName("containerChart");
+						for (i = 0; i < 5; i++){
+							y[i].style.display = 'none';
+						}
+						document.getElementById(x).style.display = 'block';
+						
+					}
+				</script>
 	</head>
 	<body>
 		<script src="https://code.highcharts.com/highcharts.js"></script>
@@ -26,65 +37,102 @@
 		if(!isset($_SESSION['username'])) {
 			$_SESSION['error'] = 'Please log in before accessing data pages.';
 			$_SESSION['redirect'] = basename($_SERVER['PHP_SELF']);
-			exit(header("Location:http://localhost/cweb2101-consultingsite/login.php"));
+			exit(header("Location:http://localhost/full-php-master/full-php-master/login.php"));
 		} else { ?>
 		<article>
-			<table>
-				<h1>Woodbury Senior High School</h1>
-				<tr>
-					<th class="tblehd">Tenure: Steady</th>
-					<th class="tblehd">Distance: 5.1 Miles</th>
-				</tr>
-				<tr>
-					<th class="piechart"><div id="WBGR" ></div></th>
-					<th class="piechart"><div id="WBMP" ></div></th>
-				</tr>
-			</table>
-			<table>
-				<h1>East Ridge Senior High School</h1>
-				<tr>
-					<th class="tblehd">Tenure: Grown</th>
-					<th class="tblehd">Distance: 8.5 Miles</th>
-				</tr>
-				<tr>
-					<th class="piechart"><div id="ERGrad" ></div></th>
-					<th class="piechart"><div id="ERmin" ></div></th>
-				</tr>
-			</table>
-			<table>
-				<h1>Tartan Senior High School</h1>
-				<tr>
-					<th class="tblehd">Tenure: Steady</th>
-					<th class="tblehd">Distance: 2.4 Miles</th>
-				</tr>
-				<tr>
-					<th class="piechart"><div id="TSGrad" ></div></th>
-					<th class="piechart"><div id="TSmin" ></div></th>
-				</tr>
-			</table>
-			<table>
-				<h1>Como Park Senior High School</h1>
-				<tr>
-					<th class="tblehd">Tenure: Steady</th>
-					<th class="tblehd">Distance: 8.7 Miles</th>
-				</tr>
-				<tr>
-					<th class="piechart"><div id="CPGrad" ></div></th>
-					<th class="piechart"><div id="CPmin" ></div></th>
-				</tr>
-			</table>
-			<table>
-				<h1>North Senior High School</h1>
-				<tr>
-					<th class="tblehd">Tenure: Decreased</th>
-					<th class="tblehd">Distance: 5.4 Miles</th>
-				</tr>
-				<tr>
-					<th class="piechart"><div id="NSGrad" ></div></th>
-					<th class="piechart"><div id="NSmin" ></div></th>
-				</tr>
-			</table>
+			<button type="button" onclick="myFunction()">Select School</button>
+			<select id="SchoolDropDown">
+				<option value="WBSHS">
+					Woodbury Senior High School
+				</option>
+				<option value="ERSHS">
+					East Ridge Senior High School
+				</option>
+				<option value="TSHS">
+						Tartan Senior High School
+				</option>
+				<option value="CPSHS">
+					<h1>Como Park Senior High School</h1>
+				</option>
+				<option value="NSHS">
+					North Senior High School
+				</option>
+			</select>
 			
+			
+			<div id="demo">
+			
+				<div class="containerChart" id="containerWBSHS" style="display:none;">
+					<h1>Woodbury Senior High School</h1>
+					<table>
+						<tr>
+							<th class="tblehd">Tenure: Steady</th>
+							<th class="tblehd">Distance: 5.1 Miles</th>
+						</tr>
+						<tr>
+							<th class="piechart"><div id="WBGR" ></div></th>
+							<th class="piechart"><div id="WBMP" ></div></th>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="containerChart" id="containerERSHS" style="display:none;">
+					<table>
+						<h1>East Ridge Senior High School</h1>
+						<tr>
+							<th class="tblehd">Tenure: Grown</th>
+							<th class="tblehd">Distance: 8.5 Miles</th>
+						</tr>
+						<tr>
+							<th class="piechart"><div id="ERGrad" ></div></th>
+							<th class="piechart"><div id="ERmin" ></div></th>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="containerChart" id="containerTSHS" style="display:none;">
+					<table>
+						<h1>Tartan Senior High School</h1>
+						<tr>
+							<th class="tblehd">Tenure: Steady</th>
+							<th class="tblehd">Distance: 2.4 Miles</th>
+						</tr>
+						<tr>
+							<th class="piechart"><div id="TSGrad" ></div></th>
+							<th class="piechart"><div id="TSmin" ></div></th>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="containerChart" id="containerCPSHS" style="display:none;">
+					<table>
+						<h1>Como Park Senior High School</h1>
+						<tr>
+							<th class="tblehd">Tenure: Steady</th>
+							<th class="tblehd">Distance: 8.7 Miles</th>
+						</tr>
+						<tr>
+							<th class="piechart"><div id="CPGrad" ></div></th>
+							<th class="piechart"><div id="CPmin" ></div></th>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="containerChart" id="containerNSHS" style="display:none;">
+					<table>
+						<h1>North Senior High School</h1>
+						<tr>
+							<th class="tblehd">Tenure: Decreased</th>
+							<th class="tblehd">Distance: 5.4 Miles</th>
+						</tr>
+						<tr>
+							<th class="piechart"><div id="NSGrad" ></div></th>
+							<th class="piechart"><div id="NSmin" ></div></th>
+						</tr>
+					</table>
+				</div>
+				
+			</div>
 		</article>
 		<?php }
 		include 'includes\footer.php' ?>
